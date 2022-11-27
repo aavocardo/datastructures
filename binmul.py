84b21b21a55729d783eacdddcf5ca6ab0fe5012a
Ricardo Saad <ricardo.saad@proton.me>
2022-11-27 13:15:19 +0200

    +binmul

--- binmul.py
+++ binmul.py
@@ -4,6 +4,7 @@
 ############################################################
 ############################################################
 def binary_multiply(top: int = 1011, bottom: int = 1111) -> int:
+    result: list = []
     product: int = 0
     remainder: int = 0
     first_product: list = []
@@ -12,18 +13,20 @@ def binary_multiply(top: int = 1011, bottom: int = 1111) -> int:
 
     for bottom_ in range(1, len(bottom) + 1):
         for top_ in range(1, len(top) + 1):
-            print(bottom[-bottom_]*top[-top_])
+            result.append(bottom[-bottom_]*top[-top_])
+            # print(bottom[-bottom_]*top[-top_])
 
+    print(result)
 
-    return product
+    if top or bottom == 0:
+        return 0
+    else:
+        return product
 
 
 def main() -> None:
+    binary_multiply(82103, 69457)
     # binary_multiply()
-
-    entry1: int = 82103
-    entry2: int = 69457
-    binary_multiply(entry1, entry2)
 
 
 if __name__ == '__main__':
