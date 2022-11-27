0cc0e03e6771db05199d9c5b75558feeabe805a1
Ricardo Saad <ricardo.saad@proton.me>
2022-11-27 20:00:13 +0200

    +binmul

--- lldj.py
+++ lldj.py
@@ -1,2 +1,10 @@
 import random
 
+
+def lottery(minimum, maximum, count):
+    results = [random.randint(minimum, maximum) for _ in range(count)]
+    results.sort()
+    return results
+
+
+print(lottery(1, 42, 6))
